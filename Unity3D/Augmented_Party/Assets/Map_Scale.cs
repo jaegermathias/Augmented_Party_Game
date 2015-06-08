899982cd9f5ba503581bd94070d9a89a8d062b43
nerdnuss <nerd.beere@googlemail.com>
2015-06-08 21:50:32 +0200

    hinzufuegen von Spheres, Materials und veraenderungen am Map_Scale skript

--- Unity3D/Augmented_Party/Assets/Map_Scale.cs
+++ Unity3D/Augmented_Party/Assets/Map_Scale.cs
@@ -3,22 +3,26 @@ using System.Collections;
 
 public class Map_Scale : MonoBehaviour 
 {
-    public GameObject Target;
-    
-	// Use this for initialization
+    // Use this for initialization
+    public GameObject Target;                                                                   // wird im Editor auf das Marker GO verlinkt
+    public float xFaktor, zFaktor;                                                              // Skalierungsfaktoren editierbar im Editor Fenster (Live)
+	
 	void Start () 
     {
-       // transform.localScale = new Vector3(health / 100, transform.localScale.y, transform.localScale.z);
+        xFaktor = 5;
+        zFaktor = 5;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        Debug.Log(Target.transform.position.x);
-
+        Debug.Log("Marker Pos X: " + Target.transform.position.x + " Z: " + Target.transform.position.z);  // Consolenausgabe: Position Marker 
+        Debug.Log("Plane  Pos X: " + transform.position.x + " Z: " + transform.position.z);           // Consolenausgabe: Position Plane
+        Debug.Log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         var tx = Target.transform.position.x;
-        var ty = Target.transform.position.y;
+        //var ty = Target.transform.position.y;                                                 // Höhe wird nicht benötigt
         var tz = Target.transform.position.z;
-        transform.localScale = new Vector3(Mathf.Abs(tz)/10, Mathf.Abs(tz)/10, Mathf.Abs(tz)/10);
+
+       transform.localScale = new Vector3((tx / xFaktor), 1, (tz / zFaktor));                   //ändern des Plane Scale anhand der Marker Position/ durch einen Faktor
 	}
 }
