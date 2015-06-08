bf2dd5cad50f4198fa99b4fbe4ee88af1cb603da
Nerdnuss <nerd.beere@gmail.com>
2015-06-08 19:34:45 +0200

    Hinzufügen eines Framemarkers und beginn eines skriptes zur Map scalierung

---
+++ Unity3D/Augmented_Party/Assets/Map_Scale.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map_Scale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
