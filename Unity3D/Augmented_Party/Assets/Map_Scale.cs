e0f34b3e2a2606173e40a787463d1b69aa1570ca
nerdnuss <nerd.beere@googlemail.com>
2015-06-08 20:14:56 +0200

    Live-Skalierung der Plane anhand des Z wertes des FrameMarkers

--- Unity3D/Augmented_Party/Assets/Map_Scale.cs
+++ Unity3D/Augmented_Party/Assets/Map_Scale.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Map_Scale : MonoBehaviour {
-
+public class Map_Scale : MonoBehaviour 
+{
+    public GameObject Target;
+    
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+       // transform.localScale = new Vector3(health / 100, transform.localScale.y, transform.localScale.z);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        Debug.Log(Target.transform.position.x);
+
+        var tx = Target.transform.position.x;
+        var ty = Target.transform.position.y;
+        var tz = Target.transform.position.z;
+        transform.localScale = new Vector3(Mathf.Abs(tz)/10, Mathf.Abs(tz)/10, Mathf.Abs(tz)/10);
 	}
 }
