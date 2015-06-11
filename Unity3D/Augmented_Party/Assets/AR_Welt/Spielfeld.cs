95bca60aed4119c0b775a8984832f87ee2e14e9d
jaegermathias <jaeger.mathias@googlemail.com>
2015-06-11 19:23:21 +0200

    Mesh für das Spielfeld mit Beispiel-Anpassung eines Vertices
    Das blaue Mesh müsste in seiner Struktur dem geplanten Aufbau
    entsprechen. Beispielhaft wurde ein Vertice ueber Zeit animiert.
    Aktuell kommt es zu einem Crash, wenn der Collider unter dem
    WheelCollider in einer Update()-Methode geändert wird. Workaround ist
    der Gebrauch von FixedUpdate(). Siehe Skript: Spielfeld.cs

---
+++ Unity3D/Augmented_Party/Assets/AR_Welt/Spielfeld.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spielfeld : MonoBehaviour {
+	void FixedUpdate() {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		Vector3[] normals = mesh.normals;
+		int i = 0;
+		while (i < vertices.Length) {
+			vertices[1].x += (float) 0.005 * Mathf.Sin(Time.time);
+			vertices[1].z -= (float) 0.005 * Mathf.Sin(Time.time);
+			i++;
+		}
+		mesh.vertices = vertices;
+
+		GetComponent<MeshCollider>().sharedMesh = null;
+		GetComponent<MeshCollider>().sharedMesh = mesh;
+
+	}
+}
