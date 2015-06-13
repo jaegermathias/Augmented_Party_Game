cccceba8ec9ddfefe1316f54c0b0a4e06b759531
ochstobi <ochs.tobi@gmail.com>
2015-06-13 16:33:50 +0200

    Respawn des Autos
    Auto wird auf wieder in die Mitte gespawnt

---
+++ Unity3D/Augmented_Party/Assets/Standard Assets/Vehicles/Car/Scripts/Respawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject car;
+	public Vector3 pos;
+	int lives;
+	void Start () {
+		pos = new Vector3 (1, 1, 1);
+		rotation = new Quaternion();
+		lives = 4;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Car Pos y: " + transform.position.y);
+		if(transform.position.y < -10f && lives > 0) {
+			transform.position = pos;
+			lives--;
+		}
+	}
+
+}
