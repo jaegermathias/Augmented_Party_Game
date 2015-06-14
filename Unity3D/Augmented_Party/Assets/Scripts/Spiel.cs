292f6e2f167fd59b272ffdf024481f41f91bc3d1
jaegermathias <jaeger.mathias@googlemail.com>
2015-06-14 18:34:50 +0200

    Alternative Spielfeld-Mitte, Initiales Spawning ueber Virtuelle Buttons
    Ich habe eine alternative Spielfeld-Mitte entwickelt. Nach dem Start der
    Anwendung kann man nun an der gewählten Position spawnen. Funktioniert
    wesentlich besser als durch mehrere simultan zu trackende ImageMarker.
    Die Vorlage "Chips" funktioniert nun jedoch nicht mehr als Fallback!
    
    + Entfernen nicht mehr benötigter Bestandteile

---
+++ Unity3D/Augmented_Party/Assets/Scripts/Spiel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Spiel : MonoBehaviour {
+
+	public GameObject PlayerPrefab;
+
+	public System.Object Player;
+
+	public GameObject[] spawnpoints_frei;
+	public GameObject[] spawnpoints_belegt;
+
+	// Use this for initialization
+	void Start () {
+		spawnpoints_frei = GameObject.FindGameObjectsWithTag("spawnpoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void spawneSpieler(Transform vb){
+		if(Player == null){
+		// Man sollte sich noch ueberlegen wie der Spawnpunkt gewaehlt wird.
+		Transform spawnpunkt = vb;
+		this.Player = Instantiate(PlayerPrefab, (vb.position), (vb.rotation));
+		}
+	}
+
+}
--- Unity3D/Augmented_Party/Assets/Scripts/VirtualButtonEventHandler.cs
+++ Unity3D/Augmented_Party/Assets/Scripts/VirtualButtonEventHandler.cs
@@ -1,7 +1,6 @@
 /*============================================================================== 
  * Copyright (c) 2012-2014 Qualcomm Connected Experiences, Inc. All Rights Reserved. 
  * ==============================================================================*/
-
 using UnityEngine;
 using System.Collections.Generic;
 using Vuforia;
@@ -16,10 +15,10 @@ public class VirtualButtonEventHandler : MonoBehaviour,
 {
     #region PUBLIC_MEMBER_VARIABLES
 
-    /// <summary>
-    /// The materials that will be set for the teapot model
-    /// </summary>
-    public Material[] m_TeapotMaterials;
+	/// <summary>
+	/// The materials that will be set for the teapot model
+	/// </summary>
+	public Material[] m_TeapotMaterials;
 
     #endregion // PUBLIC_MEMBER_VARIABLES
 
@@ -27,8 +26,8 @@ public class VirtualButtonEventHandler : MonoBehaviour,
 
     #region PRIVATE_MEMBER_VARIABLES
     
-    private GameObject mTeapot;
-    private List<Material> mActiveMaterials;
+	private GameObject mTeapot;
+	private List<Material> mActiveMaterials;
 
     #endregion // PRIVATE_MEMBER_VARIABLES
 
@@ -36,21 +35,20 @@ public class VirtualButtonEventHandler : MonoBehaviour,
 
     #region UNITY_MONOBEHAVIOUR_METHODS
 
-    void Start()
-    {
-        // Register with the virtual buttons TrackableBehaviour
-        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
-        for (int i = 0; i < vbs.Length; ++i)
-        {
-            vbs[i].RegisterEventHandler(this);
-        }
+	void Start ()
+	{
+		// Register with the virtual buttons TrackableBehaviour
+		VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour> ();
+		for (int i = 0; i < vbs.Length; ++i) {
+			vbs [i].RegisterEventHandler (this);
+		}
 
-        // Get handle to the teapot object
-        //mTeapot = transform.FindChild("teapot").gameObject;
+		// Get handle to the teapot object
+		//mTeapot = transform.FindChild("teapot").gameObject;
 
-        // The list of active materials
-        mActiveMaterials = new List<Material>();
-    }
+		// The list of active materials
+		mActiveMaterials = new List<Material> ();
+	}
 
     #endregion // UNITY_MONOBEHAVIOUR_METHODS
 
@@ -58,96 +56,49 @@ public class VirtualButtonEventHandler : MonoBehaviour,
 
     #region PUBLIC_METHODS
     
-    /// <summary>
-    /// Called when the virtual button has just been pressed:
-    /// </summary>
-    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
-    {
-		vb.GetComponent<AudioSource>().Play();
-        Debug.Log("OnButtonPressed::" + vb.VirtualButtonName);
+	/// <summary>
+	/// Called when the virtual button has just been pressed:
+	/// </summary>
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb)
+	{
+		//vb.GetComponent<AudioSource>().Play();
+		//Debug.Log("OnButtonPressed::" + vb.VirtualButtonName);
 
-        if (!IsValid())
-        {
-            return;
-        }
-
-        // Add the material corresponding to this virtual button
-        // to the active material list:
-        switch (vb.VirtualButtonName)
-        {
-            case "Play":
-			Debug.Log("Play");
-
-			//mActiveMaterials.Add(m_TeapotMaterials[0]);
-                break;
-
-            case "Pause":
-			Debug.Log("Pause");    
-                //mActiveMaterials.Add(m_TeapotMaterials[1]);
-                break;
-
-            case "Restart":
-			Debug.Log("Restart");    
-                //mActiveMaterials.Add(m_TeapotMaterials[2]);
-                break;
-		case "Select":
-			Debug.Log("Select");    
-			//mActiveMaterials.Add(m_TeapotMaterials[2]);
+		// Add the material corresponding to this virtual button
+		// to the active material list:
+		switch (vb.VirtualButtonName) {
+		case "Spawn1":
+			Debug.Log("Spawn1");
 			break;
-        }
 
-        // Apply the new material:
-        if (mActiveMaterials.Count > 0)
-            mTeapot.GetComponent<Renderer>().material = mActiveMaterials[mActiveMaterials.Count - 1];
-    }
+		case "Spawn2":
+			Debug.Log ("Spawn2");    
+			break;
 
+		case "Spawn3":
+			Debug.Log ("Spawn3");    
+			break;
+		case "Spawn4":
+			Debug.Log ("Spawn4");    
+			break;
+		}
 
-    /// <summary>
-    /// Called when the virtual button has just been released:
-    /// </summary>
-    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
-    {
-        if (!IsValid())
-        {
-            return;
-        }
-
-        // Remove the material corresponding to this virtual button
-        // from the active material list:
-        switch (vb.VirtualButtonName)
-        {
-            case "red":
-                mActiveMaterials.Remove(m_TeapotMaterials[0]);
-                break;
-
-            case "blue":
-                mActiveMaterials.Remove(m_TeapotMaterials[1]);
-                break;
-
-            case "yellow":
-                mActiveMaterials.Remove(m_TeapotMaterials[2]);
-                break;
-
-            case "green":
-                mActiveMaterials.Remove(m_TeapotMaterials[3]);
-                break;
-        }
-
-        // Apply the next active material, or apply the default material:
-        if (mActiveMaterials.Count > 0)
-            mTeapot.GetComponent<Renderer>().material = mActiveMaterials[mActiveMaterials.Count - 1];
-        else
-            mTeapot.GetComponent<Renderer>().material = m_TeapotMaterials[4];
-    }
+		GameObject Spiel = GameObject.FindGameObjectWithTag ("spielsteuerung");
+		Spiel.SendMessage ("spawneSpieler", vb.transform);
+		
+		// Apply the new material:
+		if (mActiveMaterials.Count > 0)
+			mTeapot.GetComponent<Renderer> ().material = mActiveMaterials [mActiveMaterials.Count - 1];
+	}
 
 
-    private bool IsValid()
-    {
-        // Check the materials and teapot have been set:
-        return  m_TeapotMaterials != null &&
-                m_TeapotMaterials.Length == 5 &&
-                mTeapot != null;
-    }
+	/// <summary>
+	/// Called when the virtual button has just been released:
+	/// </summary>
+	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb)
+	{
+
+	}
 
     #endregion // PUBLIC_METHODS
 }
