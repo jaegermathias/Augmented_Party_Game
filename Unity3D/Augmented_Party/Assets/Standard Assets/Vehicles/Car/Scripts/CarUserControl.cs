1e441d064c0b28a24dfe946baac23c3ed7c2a2fb
jaeger <jaeger.mathias@googlemail.com>
2015-06-16 13:18:08 +0200

    Netzwerk-Updatetest05
    Network Transform an Player Prefab hinzugefuegt und die fehlenden
    Spawnpoints ergaenzt.

--- Unity3D/Augmented_Party/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Unity3D/Augmented_Party/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -5,30 +5,32 @@ using UnityEngine.Networking;
 
 namespace UnityStandardAssets.Vehicles.Car
 {
-    [RequireComponent(typeof (CarController))]
+	[RequireComponent(typeof(CarController))]
 	public class CarUserControl : NetworkBehaviour
-    {
-        private CarController m_Car; // the car controller we want to use
+	{
+		private CarController m_Car; // the car controller we want to use
 
 
-        private void Awake()
-        {
-            // get the car controller
-            m_Car = GetComponent<CarController>();
-        }
+		private void Awake ()
+		{
+			// get the car controller
+			m_Car = GetComponent<CarController> ();
+		}
 
-
-        private void FixedUpdate()
-        {
-            // pass the input to the car!
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
+		private void FixedUpdate ()
+		{
+			if (!isLocalPlayer) {
+				return;
+			}
+				// pass the input to the car!
+				float h = CrossPlatformInputManager.GetAxis ("Horizontal");
+				float v = CrossPlatformInputManager.GetAxis ("Vertical");
 #if !MOBILE_INPUT
-            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
-            m_Car.Move(h, v, v, handbrake);
+				float handbrake = CrossPlatformInputManager.GetAxis ("Jump");
+				m_Car.Move (h, v, v, handbrake);
 #else
             m_Car.Move(h, v, v, 0f);
 #endif
-        }
-    }
+		}
+	}
 }
