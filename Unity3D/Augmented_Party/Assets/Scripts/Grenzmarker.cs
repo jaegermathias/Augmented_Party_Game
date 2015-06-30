01706bd979f1f1b5123b816deab68d793dfe8e74
Nerdnuss <nerd.beere@googlemail.com>
2015-06-30 16:28:29 +0200

    Versuch mit einem 2. Imagemarker zur Spielfeld-Begrenzung
    WIP

---
+++ Unity3D/Augmented_Party/Assets/Scripts/Grenzmarker.cs
@@ -0,0 +1,74 @@
+﻿/*============================================================================== 
+ * Copyright (c) 2012-2014 Qualcomm Connected Experiences, Inc. All Rights Reserved. 
+ * ==============================================================================*/
+
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// A custom handler that implements the ITrackableEventHandler interface.
+/// </summary>
+public class Grenzmarker : MonoBehaviour,
+                                            ITrackableEventHandler
+{
+    #region PUBLIC_MEMBER_VARIABLES
+    public bool isBeingTracked;
+    public int ID;
+    #endregion PUBLIC_MEMBER_VARIABLES
+
+    #region PRIVATE_MEMBER_VARIABLES
+    private TrackableBehaviour mTrackableBehaviour;
+    #endregion // PRIVATE_MEMBER_VARIABLES
+
+    #region PUBLIC_METODS
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+
+    /// <summary>
+    /// Implementation of the ITrackableEventHandler function called when the
+    /// tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else
+        {
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+
+
+    #region PRIVATE_METHODS
+    private void OnTrackingFound()
+    {
+        isBeingTracked = true;
+        Debug.Log("Fuuuuuuuuuuuuuuuuuuuuuuuu " + this.ID);
+        
+    }
+
+
+    private void OnTrackingLost()
+    {
+        isBeingTracked = false;
+
+    }
+
+    #endregion // PRIVATE_METHODS
+}
