59ee4b8af5fdbd7fe7a80dbddd6487558e12592d
jaegermathias <jaeger.mathias@googlemail.com>
2015-06-30 17:42:07 +0200

    Initialer Rebuild
    Grundlegende Netzwerkfunktionalitaet mit Fahrzeug als Player Prefab.
    Spawning an unterschiedlichen Punkten funktioniert. Kein Vuforia. Kein
    Matchmaker.

--- Unity3D/Augmented_Party/Assets/ZusaetzlicheCarInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ZusaetzlicheCarInfo : MonoBehaviour {
-	//Diese Klasse hält zusätzliche Informationen zum Car
-	public int leben = 4;
-	public Color farbe;
-	public Vector3 spawn;
-}
