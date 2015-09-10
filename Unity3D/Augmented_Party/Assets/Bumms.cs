d7e1369e5f72fb297d6739dbd7c10d9b43b68ac0
nerdnuss <nerd.beere@googlemail.com>
2015-09-10 21:43:18 +0200

    Beginn Bouncing Skript für die Kollision der Fahrzeuge
    Relatiert zu Issue #30

---
+++ Unity3D/Augmented_Party/Assets/Bumms.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bumms : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    
+    }
+    void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.name == "PlayerDINGENS")
+            {
+            
+           // other.gameObject.Rigidbody.AddForce(0,0,1);
+            }
+    }
+}
