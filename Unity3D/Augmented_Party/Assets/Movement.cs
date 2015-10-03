75b15bbd601c1ee913ce4f10291f31489f9b5df5
nerdnuss <nerd.beere@googlemail.com>
2015-10-03 15:40:10 +0200

    Kreuz Modell mit Bewegungsanimation eingebaut
    Rotes halbtransparentes Kreuz eingebaut welches am Objektmarker
    platziert wird und um sich selbst rotiert (und leicht auf und ab
    Hovert). Animation per global Variable deaktivierbar (Std. für
    Entwicklungszwecke: false)
    
    Relatiert: #50

---
+++ Unity3D/Augmented_Party/Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public int geschwindigkeit;
+    public bool animation;
+    // Use this for initialization
+    void Start()
+    {
+        geschwindigkeit = 5;
+        animation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (animation)
+        {
+            //Rotation Kreuz
+            transform.Rotate(0, 0, Time.deltaTime + geschwindigkeit);
+            transform.Rotate(0, Time.deltaTime, 0, Space.World);
+
+            //Hover effect kreuz
+            float ausschlag = 0.50f;
+            float geschwindigkeit = 0.50f;
+            transform.position += ausschlag * (Mathf.Sin(2 * Mathf.PI * geschwindigkeit * Time.time) - Mathf.Sin(2 * Mathf.PI * geschwindigkeit * (Time.time - Time.deltaTime))) * transform.forward;
+        }
+    }
+}
